26fe5d7e6605059f7ce35966740085260b76c417
leonjaques <leonardomjq@gmail.com>
2020-09-30 13:12:38 +0100

    meta tags changes

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,14 @@ export default function Home() {
         {/* Twitter */}
         <meta name="twitter:card" content="summary" key="twcard" />
         <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+        {/* Discord */}
+        <meta name="discord:card" content="summary" key="twcard" />
+        <meta name="discord:creator" content={twitterHandle} key="twhandle" />
+        <meta
+          property="discord:image"
+          content="https://raw.githubusercontent.com/leonardomjq/Newsletter-nextjs/master/public/bubblegum2.png"
+          key="discordimage"
+        />
         {/* Open Graph */}
         <meta property="og:url" content={currentURL} key="ogurl" />
         <meta
